cf872ac009eefd48475be54329cab6e0aa9192d0
Mika Attila <radiantstatue@gmail.com>
2016-04-10 19:05:26 +0200

    Have a readline-like interface for boncactl

--- src/bin/boncactl.rs
+++ src/bin/boncactl.rs
@@ -1,5 +1,8 @@
 extern crate zmq;
-use std::io::prelude::*;
+extern crate rustyline;
+
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
 
 fn main() {
     let mut zmq_ctx = zmq::Context::new();
@@ -7,19 +10,21 @@ fn main() {
     let command_str = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
     sock.connect("ipc:///tmp/boncarobot.sock").unwrap();
     if command_str.is_empty() {
-        let stdin = std::io::stdin();
-        let mut lines = stdin.lock().lines();
+        let mut editor = Editor::new();
         loop {
-            print!("> ");
-            std::io::stdout().flush().unwrap();
-            if let Some(l) = lines.next() {
-                sock.send_str(&l.unwrap(), zmq::DONTWAIT).unwrap();
-            } else {
-                println!("Goodbye!");
-                return;
+            match editor.readline("> ") {
+                Ok(line) => {
+                    sock.send_str(&line, zmq::DONTWAIT).unwrap();
+                    editor.add_history_entry(&line);
+                }
+                Err(e) => {
+                    match e {
+                        ReadlineError::Eof | ReadlineError::Interrupted => {}
+                        _ => panic!("error: {}", e),
+                    }
+                    return;
+                }
             }
         }
-    } else {
-        sock.send_str(&command_str, zmq::DONTWAIT).unwrap();
     }
 }
