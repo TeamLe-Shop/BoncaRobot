7a96acaa0cc404ca736ca9d0c88a2ca833327439
Mika Attila <radiantstatue@gmail.com>
2017-01-25 19:16:49 +0100

    Don't worry Jenkins
    I'VE GOT THIS

--- plugins/search/src/lib.rs
+++ plugins/search/src/lib.rs
@@ -19,13 +19,33 @@ pub fn query_google(query: &str) -> Result<String, Box<Error>> {
     Ok(String::from_utf8_lossy(&body).into_owned())
 }
 
+const URLQ: &'static str = "/url?q=";
+
+fn parse_urlq(urlq: &str) -> Result<&str, Box<Error>> {
+    let begin = URLQ.len();
+    let end = begin + urlq[begin..].find("&sa=").ok_or("Expected &sa= shit, but didn't find it.")?;
+    Ok(&urlq[begin..end])
+}
+
+fn parse_href(href: &str) -> Result<&str, Box<Error>> {
+    if href.starts_with(URLQ) {
+        parse_urlq(href)
+    } else {
+        Ok(href)
+    }
+}
+
 pub fn parse_first_result(body: &str) -> Result<String, Box<Error>> {
     use scraper::{Html, Selector};
 
-    let html = Html::parse_document(&body);
-    let sel = Selector::parse("cite").unwrap();
-    let cite = html.select(&sel).next().ok_or("There should be a <cite>, but there isn't")?;
-    Ok(cite.text().next().ok_or("Wat. <cite> has no text.")?.to_owned())
+    let html = Html::parse_document(body);
+    let sel = Selector::parse("h3.r").unwrap();
+    let h3 = html.select(&sel).next().ok_or("There should be a h3 class=\"r\", but there isn't")?;
+    let sel = Selector::parse("a").unwrap();
+    let a = h3.select(&sel).next().ok_or("There should be a <a>, but there isn't")?;
+    let href = a.value().attr("href").ok_or("<a> should have a href, but it doesn't")?;
+    let href = url::percent_encoding::percent_decode(href.as_bytes()).decode_utf8()?;
+    Ok(parse_href(&href)?.to_owned())
 }
 
 #[test]
