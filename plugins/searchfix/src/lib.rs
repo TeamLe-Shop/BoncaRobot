ea1c88f04dc9b8809ab8cb716ec9b56ddf828e05
Mika Attila <radiantstatue@gmail.com>
2017-03-13 11:13:46 +0100

    searchfix: Don't include anything after a space in the result
    Spaces in urls aren't valid anyway

--- plugins/searchfix/src/lib.rs
+++ plugins/searchfix/src/lib.rs
@@ -13,7 +13,12 @@ impl Plugin for SearchFixPlugin {
     fn channel_msg(&mut self, msg: &str, ctx: Context) {
         let beginning = "/l/?kh=-1&uddg=";
         if let Some(idx) = msg.find(beginning) {
-            let url = &msg[idx + beginning.len()..];
+            let url_begin_index = idx + beginning.len();
+            let url_end_index = msg[url_begin_index..]
+                .find(" ")
+                .map(|idx| url_begin_index + idx)
+                .unwrap_or(msg.len());
+            let url = &msg[url_begin_index..url_end_index];
             let decoded = url::percent_encoding::percent_decode(url.as_bytes())
                 .decode_utf8()
                 .unwrap_or_else(|_| "invalid url".into());
