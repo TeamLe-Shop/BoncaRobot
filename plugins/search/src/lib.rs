bbd442c8bc9fb15a9785617535e3941d0d575263
Mika Attila <radiantstatue@gmail.com>
2017-07-03 09:51:30 +0200

    Kekking rustfmt

--- plugins/search/src/lib.rs
+++ plugins/search/src/lib.rs
@@ -27,9 +27,9 @@ const URLQ: &'static str = "/url?q=";
 fn parse_urlq(urlq: &str) -> Result<&str, Box<Error>> {
     let begin = URLQ.len();
     let end = begin +
-        urlq[begin..].find("&sa=").ok_or(
-            "Expected &sa= shit, but didn't find it.",
-        )?;
+        urlq[begin..]
+            .find("&sa=")
+            .ok_or("Expected &sa= shit, but didn't find it.")?;
     Ok(&urlq[begin..end])
 }
 
@@ -48,16 +48,15 @@ pub fn parse_first_result(body: &str) -> Result<String, Box<Error>> {
     let sel = Selector::parse("h3.r").unwrap();
     let mut h3s = html.select(&sel);
     loop {
-        let h3 = h3s.next().ok_or(
-            "There should be a h3 class=\"r\", but there isn't",
-        )?;
+        let h3 = h3s.next()
+            .ok_or("There should be a h3 class=\"r\", but there isn't")?;
         let sel = Selector::parse("a").unwrap();
-        let a = h3.select(&sel).next().ok_or(
-            "There should be a <a>, but there isn't",
-        )?;
-        let href = a.value().attr("href").ok_or(
-            "<a> should have a href, but it doesn't",
-        )?;
+        let a = h3.select(&sel)
+            .next()
+            .ok_or("There should be a <a>, but there isn't")?;
+        let href = a.value()
+            .attr("href")
+            .ok_or("<a> should have a href, but it doesn't")?;
         let href = url::percent_encoding::percent_decode(href.as_bytes())
             .decode_utf8()?;
         if href.starts_with("/search?q=") {
@@ -72,10 +71,8 @@ struct SearchPlugin;
 impl SearchPlugin {
     fn search(_this: &mut Plugin, arg: &str, ctx: Context) {
         if arg.is_empty() {
-            let _ = ctx.irc.privmsg(
-                ctx.channel.name(),
-                "You need to search for something bro.",
-            );
+            let _ = ctx.irc
+                .privmsg(ctx.channel.name(), "You need to search for something bro.");
             return;
         }
         match query_google(arg) {
@@ -85,18 +82,14 @@ impl SearchPlugin {
                         let _ = ctx.irc.privmsg(ctx.channel.name(), &result);
                     }
                     Err(e) => {
-                        let _ = ctx.irc.privmsg(
-                            ctx.channel.name(),
-                            &format!("Error: {}", e),
-                        );
+                        let _ = ctx.irc
+                            .privmsg(ctx.channel.name(), &format!("Error: {}", e));
                     }
                 }
             }
             Err(e) => {
-                let _ = ctx.irc.privmsg(
-                    ctx.channel.name(),
-                    &format!("Error when googuring: {}", e),
-                );
+                let _ = ctx.irc
+                    .privmsg(ctx.channel.name(), &format!("Error when googuring: {}", e));
             }
         }
     }
