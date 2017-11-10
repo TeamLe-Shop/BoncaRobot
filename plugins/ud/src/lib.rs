f684a04a49b650bd74a56e393fa1e24f783f2c22
Mika Attila <radiantstatue@gmail.com>
2017-11-10 22:08:12 +0100

    Add udn command for specific entry

--- plugins/ud/src/lib.rs
+++ plugins/ud/src/lib.rs
@@ -2,10 +2,12 @@ extern crate json;
 #[macro_use]
 extern crate plugin_api;
 extern crate reqwest;
+extern crate split_whitespace_rest;
 
 use plugin_api::prelude::*;
 use std::error::Error;
 use std::io::prelude::*;
+use split_whitespace_rest::SplitWhitespace;
 
 pub fn query(query: &str) -> Result<String, Box<Error>> {
     let msg = format!(
@@ -27,7 +29,28 @@ pub fn query(query: &str) -> Result<String, Box<Error>> {
 struct UdPlugin;
 
 impl UdPlugin {
+    fn udn(_this: &mut Plugin, arg: &str, ctx: Context) {
+        let mut sw = SplitWhitespace::new(arg);
+        let entry = match sw.next() {
+            Some(en) => en,
+            None => {
+                ctx.send_channel("Need entry num bro.");
+                return;
+            }
+        };
+        let entry: usize = match entry.parse() {
+            Ok(n) => n,
+            Err(_) => {
+                ctx.send_channel("Very clever troll bro. Gimme number for entry plox.");
+                return;
+            }
+        };
+        Self::udlookup(sw.rest_as_slice(), entry, ctx);
+    }
     fn ud(_this: &mut Plugin, arg: &str, ctx: Context) {
+        Self::udlookup(arg, 0, ctx);
+    }
+    fn udlookup(arg: &str, entry: usize, ctx: Context) {
         if arg.is_empty() {
             ctx.send_channel("You need to search for something bro.");
             return;
@@ -41,7 +64,7 @@ impl UdPlugin {
                         return;
                     }
                 };
-                let mut entry = match json["list"][0]["definition"].as_str() {
+                let mut entry = match json["list"][entry]["definition"].as_str() {
                     Some(entry) => entry,
                     None => {
                         ctx.send_channel("ENGLISH, MOTHERFUCKER.");
@@ -75,6 +98,7 @@ impl Plugin for UdPlugin {
     }
     fn register(&self, meta: &mut PluginMeta) {
         meta.command("ud", "Urban dictionary lookup", Self::ud);
+        meta.command("udn", "Urban dictionary lookup (entry n)", Self::udn);
     }
 }
 
