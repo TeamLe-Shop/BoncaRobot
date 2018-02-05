ba6080e6a63ef91ee26184f2f84f18d0a026df6a
Mika Attila <radiantstatue@gmail.com>
2018-02-05 09:36:36 +0100

    Show HTTP status when HTTP request errors

--- plugin-libs/http-request-common/src/lib.rs
+++ plugin-libs/http-request-common/src/lib.rs
@@ -6,8 +6,10 @@ use std::io::Read;
 pub fn fetch_string(link: &str) -> Result<String, Box<Error>> {
     let mut resp = reqwest::get(link)?;
 
-    if !resp.status().is_success() {
-        return Err("Something went wrong with the request".into());
+    let status = resp.status();
+
+    if !status.is_success() {
+        return Err(status.to_string().into());
     }
 
     let mut content = Vec::new();
