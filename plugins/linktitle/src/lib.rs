3650cb93272c93925a3c190284d33073b6f9c4a8
Mika Attila <radiantstatue@gmail.com>
2018-04-12 17:56:42 +0200

    Attempt 2

--- plugins/linktitle/src/lib.rs
+++ plugins/linktitle/src/lib.rs
@@ -42,7 +42,7 @@ impl Plugin for LinkTitlePlugin {
     fn channel_msg(&mut self, msg: &str, ctx: Context) {
         use regex::Regex;
         lazy_static! {
-            static ref RE: Regex = Regex::new(r#"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"#).unwrap();
+            static ref RE: Regex = Regex::new(r#"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"#).unwrap();
         }
         if let Some(cap) = RE.captures_iter(msg).next() {
             let title = get_title(&cap[0]);
