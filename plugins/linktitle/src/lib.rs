d6f18a0b0f494186a26e1d39ac51b8d28f76f9ad
Mika Attila <radiantstatue@gmail.com>
2018-04-12 19:08:16 +0200

    Kek no need for lazy_static in linktitle

--- plugins/linktitle/src/lib.rs
+++ plugins/linktitle/src/lib.rs
@@ -1,25 +1,26 @@
 #[macro_use]
 extern crate plugin_api;
 extern crate regex;
-#[macro_use]
-extern crate lazy_static;
 extern crate titlefetch;
 
 use plugin_api::prelude::*;
+use regex::Regex;
 use titlefetch::get_title;
+const RE: &str =
+    r#"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"#;
 
-struct LinkTitlePlugin;
+struct LinkTitlePlugin {
+    regex: Regex,
+}
 
 impl Plugin for LinkTitlePlugin {
     fn new() -> Self {
-        LinkTitlePlugin
+        Self {
+            regex: Regex::new(RE).unwrap(),
+        }
     }
     fn channel_msg(&mut self, msg: &str, ctx: Context) {
-        use regex::Regex;
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r#"https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"#).unwrap();
-        }
-        if let Some(cap) = RE.captures_iter(msg).next() {
+        if let Some(cap) = self.regex.captures_iter(msg).next() {
             let title = get_title(&cap[0]);
             if !title.is_empty() {
                 ctx.send_channel(&title);
